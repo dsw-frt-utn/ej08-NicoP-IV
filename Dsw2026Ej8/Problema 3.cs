a02f99d91b8eeda9ed955285420bee07a2176e34
Nicolas Perry <nicolasperry04@gmail.com>
2026-05-03 20:52:01 -0300

    Agrego la solucion propuesta para el Problema 3

---
+++ Dsw2026Ej8/Problema 3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        public string? Descripcion { get; set; }
+        public void ModificarDescripcion(string nuevaDesc) => Descripcion = nuevaDesc;
+    }
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int valorDeLaCopia = originalValue;
+            valorDeLaCopia++;
+
+            Product copiaReferencia = product;
+            copiaReferencia.ModificarDescripcion("Copia Modificada");
+
+            return $"{originalValue}-{valorDeLaCopia}-{product.Descripcion}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -9,10 +9,19 @@
             //Console.WriteLine(resultado);
 
             //PROBLEMA 2, VALORES DE PRUEBA
-            Problema_2 p2 = new Problema_2();
-            Console.WriteLine("1: " + p2.CrearResumenVenta(606, "Procesador", 1, 290000));
-            Console.WriteLine("2: " + p2.CrearResumenVenta(608, "Mouse", -2, 8000));
-            Console.WriteLine("3: " + p2.CrearResumenVenta(608, "Parlante", 4, 1500));
+            //Problema_2 p2 = new Problema_2();
+            //Console.WriteLine("1: " + p2.CrearResumenVenta(606, "Procesador", 1, 290000));
+            //Console.WriteLine("2: " + p2.CrearResumenVenta(608, "Mouse", -2, 8000));
+            //Console.WriteLine("3: " + p2.CrearResumenVenta(608, "Parlante", 4, 1500));
+
+            //PROBLEMA 3, VALORES DE PRUEBA
+            int nuevoNumero = 1;
+            Product nuevoProducto = new Product { Descripcion = "Ultimo Modelo" };
+            Problema3 p3 = new Problema3();
+            string resultado = p3.CompararCopias(nuevoNumero, nuevoProducto);
+            Console.WriteLine("Resultado: " + resultado);
+            Console.WriteLine($"El 'nuevoNumero': {nuevoNumero} (No hubo cambio.)");
+            Console.WriteLine($"El 'nuevoProducto': {nuevoProducto.Descripcion} (Hubo cambios.)");
         }
     }
 }
