a439897b5c6848f03662d65ab1c0c2f78dfbf44c
Nicolas Perry <nicolasperry04@gmail.com>
2026-05-03 20:37:06 -0300

    Agrego la solucion propuesta para el problema 2

---
+++ Dsw2026Ej8/Problema 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema_2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal totalCalculado = quantity * unitPrice;
+            if (quantity <= 0)
+            {
+                totalCalculado = 0;
+            }
+
+            var resumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = totalCalculado
+            };
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,10 +4,15 @@
     {
         static void Main(string[] args)
         {
-            //PROBLEMA 1, EJEMPLO
-            string resultado = ProductHelper.ObtenerEtiquetaProducto(60680, "Leche Entera", 1800.64m);
-            Console.WriteLine(resultado);
+            //PROBLEMA 1, VALORES DE PRUEBA
+            //string resultado = ProductHelper.ObtenerEtiquetaProducto(60680, "Leche Entera", 1800.64m);
+            //Console.WriteLine(resultado);
 
+            //PROBLEMA 2, VALORES DE PRUEBA
+            Problema_2 p2 = new Problema_2();
+            Console.WriteLine("1: " + p2.CrearResumenVenta(606, "Procesador", 1, 290000));
+            Console.WriteLine("2: " + p2.CrearResumenVenta(608, "Mouse", -2, 8000));
+            Console.WriteLine("3: " + p2.CrearResumenVenta(608, "Parlante", 4, 1500));
         }
     }
 }
