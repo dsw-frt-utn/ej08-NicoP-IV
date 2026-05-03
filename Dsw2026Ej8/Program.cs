4ab1c6f31b85235b197463f787d7b1c52ebeafb5
Nicolas Perry <nicolasperry04@gmail.com>
2026-05-03 20:20:22 -0300

    Cambio el README y propongo solucion al problema 1

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //PROBLEMA 1, EJEMPLO
+            string resultado = ProductHelper.ObtenerEtiquetaProducto(60680, "Leche Entera", 1800.64m);
+            Console.WriteLine(resultado);
+
         }
     }
 }
